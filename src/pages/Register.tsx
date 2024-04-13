eccb0c7b8228c5341bb3f177f5954c9a928e2863
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2024-04-13 19:39:16 -0300

    Home do arthur

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import {useState, SyntheticEvent} from 'react';
 import axios from 'axios';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 /*ToDo:
     Feedback para o registro
